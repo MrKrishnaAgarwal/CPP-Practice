8a8db66ce69c1094bb1664554b60d89cafc110f7
Krishna Agarwal <dmkrishna.agarwal@gmail.com>
2022-11-28 16:56:39 +0530

    cpp practice

---
+++ Datatypes/Datatypes.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 100;
+    float b = 200.5;
+    char cs = 97;
+
+    cout << " The value of a is:" << " " << a << endl;
+    cout << " The value of b is:" << " " << b << endl;
+    cout << " The value of cs is:" << " " << cs << endl;
+    
+    return 0;
+    }
+    
---
+++ If_Else If_Else/if_else.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 700000;
+    int b = 70000;
+
+    if (a < b)
+    {
+        cout << "b is greater than a" << " " << endl;
+    }
+
+    else if (a == b)
+    { 
+        cout << "a and b are equal" << " " << endl;
+    }
+
+    else 
+    { 
+        cout << "a is greater than a" << " " << endl;
+    }
+
+}
