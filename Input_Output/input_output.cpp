c9506a38f107ecd0bfd4d5b55112cc415326dad4
Krishna Agarwal <dmkrishna.agarwal@gmail.com>
2022-11-28 17:17:52 +0530

    input and output

---
+++ Input_Output/input_output.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "You entered: " << a << endl;
+    int b;
+    cout << "Enter another number: ";
+    cin >> b;
+    cout << "You entered: " << b << endl;
+    cout << "Sum of the two numbers is: " << a + b << endl;
+    return 0;
+}
